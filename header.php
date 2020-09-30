483d8ed5b76e3d5a0c27d780355492a4d1b3f41f
Dilan8 <53109200+Dilan8@users.noreply.github.com>
2020-09-30 16:18:47 +0530

    Header  Initial commit

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
+    <title>NAVIGATION</title>
+</head>
+
+<body>
+    <div class="nav-top">
+        <img src="img/slogo.png" alt="Mountains">
+
+    </div>
+
+    <nav>
+        <div class="logo">
+            <h4> Sarasavi</h4>
+        </div>
+        <ul class="nav-link">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sarasavi Publication</a></li>
+            <li><a href="#">International School BoookList</a></li>
+            <li><a href="#">Dhoa Discout Book</a></li>
+            <li><a href="#">Dhoa Discout Book</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
+   
+   
+   
+</body>
+
+</html>
