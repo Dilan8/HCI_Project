e295b15ca1806e77239405b138b80eeee8c77032
Dilan8 <53109200+Dilan8@users.noreply.github.com>
2020-09-30 16:34:05 +0530

    Add footer

---
+++ footer.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <link rel="stylesheet" href="css/owl.theme.default.min.css">
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="css/all.min.css">
+
+   <style>
+      footer {
+         display: block;
+         overflow: hidden;
+         background-color: white;
+         padding: 90px 0 0;
+         color: black;
+         border-top: 1px solid black;
+      }
+
+      footer .item form input {
+         width: 100%;
+         margin-bottom: 15px;
+         padding: 10px 10px;
+         font-size: 14px;
+         border: none;
+         height: 45px;
+         border: 3px solid #0c4da2;
+      }
+
+      .form_newsletter button {
+         border: none;
+         background: #0c4da2;
+         color: #fff;
+         font-size: 13px;
+         text-transform: uppercase;
+         width: 111px;
+         height: 43px;
+         cursor: pointer;
+      }
+
+      footer div.item.social_icon li {
+         float: left;
+         width: auto;
+      }
+
+      footer .item ul li {
+         font-weight: 300;
+         font-size: 30px;
+         width: 100%;
+         float: left;
+         margin-right: 10px;
+         margin-bottom: 3px;
+         font-size: 16px;
+         cursor: pointer;
+      }
+
+      li {
+         list-style-type: none;
+      }
+
+      footer .copyright {
+         padding: 15px 0 16px;
+         margin-top: 50px;
+         background: #0e0906;
+      }
+
+      footer .copyright p {
+         color: blanchedalmond;
+         font-size: 15px;
+      }
+   </style>
+</head>
+
+<body>
+   <footer>
+      <div class="container">
+         <div class="row">
+
+            <div class="col-lg-3 col-md-6 col-12">
+               <div class="item">
+                  <h4>Adderss</h4>
+                  <p><i class="fas fa-map-marker-alt"></i>30,Stanley Tilakaratne Mawatha,Nugegoda.<br>Nugegoda</p>
+                  <p><i class="fas fa-phone-alt"></i>(+94) 11285251</p>
+                  <p><i class="fas fa-envelope"></i> web@sarasavi.lk</p>
+               </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 col-12">
+               <div class="item">
+                  <h4>Company</h4>
+                  <ul class="opening_hour_">
+                     <li><span class="left_side">AboutUS</span></li>
+                     <li><span class="left_side">Brance Network</span></li>
+                     <li><span class="left_side">Terms of Service</span></li>
+                     <li><span class="left_side">Privacy Policy</span></li>
+
+                  </ul>
+               </div>
+            </div>
+            <div class="col-lg-2 col-md-6 col-12">
+               <div class="item">
+                  <h4>HELP</h4>
+                  <ul class="opening_hour_">
+                     <li><span class="left_side">COntactUs</span></li>
+                     <li><span class="left_side">Shipping and returns</span></li>
+                     <li><span class="left_side">Order Status</span></li>
+
+
+                  </ul>
+               </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-12">
+               <div class="item">
+                  <h4>Newsletter</h4>
+                  <form class="form_newsletter">
+                     <input type="Email" placeholder="Enter your Email" name="#" />
+                     <button>Subscribe</button>
+                  </form>
+               </div>
+               <div class="item social_icon">
+                  <ul>
+                     <li><i class="fab fa-facebook-f"></i></li>
+                     <li><i class="fab fa-twitter"></i></li>
+                     <li><i class="fab fa-linkedin"></i></li>
+                     <li><i class="fab fa-instagram"></i></li>
+                  </ul>
+               </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 col-12">
+               <div class="footer_blog_section" style="margin-top: 55px;">
+                  <img class="img-responsive" src="img/slogo.png" alt="Mountains">
+
+               </div>
+            </div>
+
+         </div>
+      </div>
+      <div class="copyright text-center">
+         <p>Copyright © 2019 Designed by <a href="https://html.design">Free Html Templates</a>. All rights reserved</p>
+      </div>
+   </footer>
+
+
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="js/owl.carousel.min.js"></script>
+   <script src="js/slideShow.js"></script>
+
+   <script src="js/app.js"></script>
