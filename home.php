58ac6b16c6e871e7a684d7d9772a5daaef776672
Dilan8 <53109200+Dilan8@users.noreply.github.com>
2020-09-30 16:47:36 +0530

    Add home page

---
+++ home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <title>Home</title>
+
+</head>
+<?php include 'header.php'; ?>
+
+<body>
+    <div class="warpper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    ` <div class="owl-one owl-carousel owl-theme">
+                        <div class="item">
+                            <img src="img/03.jpg" alt="Mountains">
+                        </div>
+                        <div class="item">
+                            <img src="img/03.jpg" alt="Mountains">
+                        </div>
+                        <div class="item">
+                            <img src="img/02.jpg" alt="Mountains">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="wrapper1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    ` <div class="owl-two owl-carousel owl-theme">
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/03.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"> <img style="padding: 10px;" src="img/03.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/02.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/02.jpg" alt="Mountains"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="wrapper2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    `<div class="owl-three owl-carousel owl-theme">
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/03.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"> <img style="padding: 10px;" src="img/03.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/02.jpg" alt="Mountains"></div>
+                        <div class="item" style="width:250px; height: 300px; border: 3px solid #0c4da2;"><img style="padding: 10px;" src="img/02.jpg" alt="Mountains"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+<?php include 'footer.php'; ?>
+
+</html>
