fed5ce841a6d4a6c5780e6273f5a5c628626990f
Cletwin <53634739+CletwinE@users.noreply.github.com>
2020-10-04 14:14:13 +0530

    test2

--- home.php
+++ home.php
@@ -164,7 +164,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="heading_section">Top 10 best selling</h2>
+                    <h2 class="heading_section">Top 10 best selling...</h2>
                     ` <div class="owl-two owl-carousel owl-theme">
                         <div class="col-lg-2 col-md-3 col-sm-4">
                             <div class="item" style="background-color: white;">
