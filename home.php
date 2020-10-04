a99342d994f1abe390ab668eb7d995aae1149b09
Cletwin <53634739+CletwinE@users.noreply.github.com>
2020-10-04 14:11:20 +0530

    test change

--- home.php
+++ home.php
@@ -85,12 +85,13 @@
         </div>
     </section>
 
-    <!--Home Page Slide Show for Top 10 best selling -->
+    <!--Home Page Slide Show for Top 10 best selling ....-->
+
     <section class="wrapper1">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="heading_section">Top 10 best selling</h2>
+                    <h2 class="heading_section">Top 10 best selling...</h2>
                     ` <div class="owl-two owl-carousel owl-theme">
                         <div class="col-lg-2 col-md-3 col-sm-4">
                             <div class="item">
