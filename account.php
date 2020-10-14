22b0a07e459a7ad0fa3e6dfb840baaf61e3dee95
varniah88 <jaanuvarni@gmail.com>
2020-10-15 00:25:59 +0530

    account setting changes

--- account.php
+++ account.php
@@ -3,72 +3,390 @@
 
 <head>
     <title>My Account</title>
+    <link rel="stylesheet" href="css/account.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
    
 </head>
 <?php include 'header.php'; ?>
 
 <body>
-    <section class="customer-details">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <h1 style="text-align: center;"> Acoount Setting</h1>
-                        </thead>
-                        <tbody>
-                            <tr>
+<section class="customer-details" style="background: linear-gradient(180deg, white,#a6d1ff, white );">
 
-                                <th class="box" scope="row">Full Name</th>
-                                <td class="text">Dilan Vasandaraj &ensp;&ensp;
-                                    <button type="button" class="btn btn-dark btn-xs edit" style="padding-left: 20px;"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-success btn-xs save" style="padding-left: 20px;"><i class="fas fa-save"></i></button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th class="box" scope="row">Email Address</th>
-                                <td class="text">vasandarajdilan64@gmail.com
-                                    <button type="button" class="btn btn-dark btn-xs edit" style="padding-left: 20px;"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-success btn-xs save" style="padding-left: 20px;"><i class="fas fa-save"></i></button>
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <th class="box" scope="row">Phone Number</th>
-                                <td class="text">0717236676
-                                    <button type="button" class="btn btn-dark btn-xs edit" style="padding-left: 20px;"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-success btn-xs save" style="padding-left: 20px;"><i class="fas fa-save"></i></button>
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <th class="box" scope="row">Password</th>
-                                <td class="text">*******
-                                    <button type="button" class="btn btn-dark btn-xs edit" style="padding-left: 20px;"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-success btn-xs save" style="padding-left: 20px;"><i class="fas fa-save"></i></button>
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row">Security Question</th>
-                                <td>
-                                    Your Birth place?
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row">Your Answer</th>
-                                <td>Batticalo</td>
-
-                            </tr>
-
-                        </tbody>
-                    </table>
-
+<div class="col-lg-12">
+    
+          <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+			
+              <div class="row align-items-center">
+			  <div class="card-profile-image">
+               
+                    <img id="myImg" src="img/avatar.png" class="rounded-circle">
+              
                 </div>
+                <div class="col-10">
+                  <h3 style="text-align: center;" class="mb-0">Account Info</h3>
+                </div>
+                <div class=" text-right">
+                  <button id="myBtn" class="btn btn-sm btn-success">Edit Profile</button>
+                </div>
+              </div>
             </div>
-        </div>
+            <div class="card-body">
+              <form>
+                <h6 class="heading-small text-muted mb-4">User information</h6>
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label dark"  for="input-username">User Name</label>
+                        <input type="text" id="input-username" readonly="true"   class="form-control form-control-alternative"  value="Dilan Vasandaraj">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-email">Email Address</label>
+                        <input type="email" id="input-email" readonly="true"  class="form-control form-control-alternative" value="vasandarajdilan64@gmail.com">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-first-name">Contact No</label>
+                        <input type="text" id="input-first-name" readonly="true"   class="form-control form-control-alternative"  value="0717236676">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Password</label>
+                        <input type="text" id="input-last-name" readonly="true"   class="form-control form-control-alternative"  value="************">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+				</form>
+</div>
+</div>
+				</div>
+				
+				<br></br>
+		<div class="row">		
+				<div class="col-md-5">
+    
+       <section class="wrapper2" style="background: linear-gradient(180deg, white, #383d42);">
+                <h3 style="text-align: center;">Account Info</h3>
+                
+                    <table class="table ">
+                     
+                   
+                        
+                            <tr>
+
+                                <td class="box" scope="row">Full Name</td>
+                                <td class="text">Dilan Vasandaraj &ensp;&ensp;</td>
+                               
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Email Address</td>
+                                <td class="text">vasandarajdilan64@gmail.com</td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Phone Number</td>
+                                <td class="text">0717236676 </td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Password</td>
+                                <td class="text">******* </td>
+                              
+                            </tr>
+
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                <button type="button" class="btn  btn-success btn-xs " ><i<i class="fas fa-pencil-alt"></i></button>
+								</td>
+								<td>
+								  <button type="button" class="btn  btn-danger btn-xs " ><i class="fas fa-trash-alt"></i></button>
+                                </td>
+                          
+                            </tr>
+                            </tbody>
+                            </table>
+                            </section>
+</div>
+			
+					<div class="col-md-2">
+			</div>
+					<div class="col-md-5">
+    
+           <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+              <div class="row align-items-center">
+                <div class="col-10">
+                  <h3 style="text-align: center;" class="mb-0">Shippig Address </h3>
+                </div>
+                
+              </div>
+            </div>
+            <div class="card-body">
+              <form>
+                <h6 class="heading-small text-muted mb-4">Shippig information</h6>
+                <div class="pl-lg-4">
+                   <table class="table ">
+                     
+                   
+                        
+                            <tr>
+
+                                <td class="box" scope="row">Full Name</td>
+                                <td class="text">Dilan Vasandaraj &ensp;&ensp;</td>
+                               
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Email Address</td>
+                                <td class="text">vasandarajdilan64@gmail.com</td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Phone Number</td>
+                                <td class="text">0717236676 </td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Password</td>
+                                <td class="text">******* </td>
+                              
+                            </tr>
+
+                           
+                            </table>
+                  </div>
+				 </form>
+                </div>
+</div>
+</div>
+</div>
+
+			<br></br>	
+					<div class="row">		
+				<div class="col-md-5">
+    
+       <section class="wrapper2" style="background: linear-gradient(180deg, white, #383d42);">
+                <h3 style="text-align: center;">Account Info</h3>
+                
+                    <table class="table ">
+                     
+                   
+                        
+                            <tr>
+
+                                <td class="box" scope="row">Full Name</td>
+                                <td class="text">Dilan Vasandaraj &ensp;&ensp;</td>
+                               
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Email Address</td>
+                                <td class="text">vasandarajdilan64@gmail.com</td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Phone Number</td>
+                                <td class="text">0717236676 </td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Password</td>
+                                <td class="text">******* </td>
+                              
+                            </tr>
+
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                <button type="button" class="btn  btn-success btn-xs " ><i<i class="fas fa-pencil-alt"></i></button>
+								</td>
+								<td>
+								  <button type="button" class="btn  btn-danger btn-xs " ><i class="fas fa-trash-alt"></i></button>
+                                </td>
+                          
+                            </tr>
+                            </tbody>
+                            </table>
+                            </section>
+</div>
+			<div class="col-md-2">
+			</div>
+					<div class="col-md-5">
+    
+          <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+              <div class="row align-items-center">
+                <div class="col-10">
+                  <h3 style="text-align: center;" class="mb-0">Shippig Address </h3>
+                </div>
+               
+              </div>
+            </div>
+            <div class="card-body">
+              <form>
+                <h6 class="heading-small text-muted mb-4">Shippig information</h6>
+                <div class="pl-lg-4">
+                   <table class="table ">
+                     
+                   
+                        
+                            <tr>
+
+                                <td class="box" scope="row">Full Name</td>
+                                <td class="text">Dilan Vasandaraj &ensp;&ensp;</td>
+                               
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Email Address</td>
+                                <td class="text">vasandarajdilan64@gmail.com</td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Phone Number</td>
+                                <td class="text">0717236676 </td>
+                              
+                            </tr>
+                            <tr>
+                                <td class="box" scope="row">Password</td>
+                                <td class="text">******* </td>
+                              
+                            </tr>
+
+                           
+                            </table>
+                  </div>
+				 </form>
+                </div>
+</div>
+</div>
+</div>
+
+<div id="myModal" class="customModal">
+
+  <!-- Modal content -->
+  <div class="customModal-content col-lg-6">
+     <section class="wrapper2" style="background: linear-gradient(180deg, white, #007bff);">
+    <span class="close">&times;</span>
+
+     <div class="card-body">
+              <form>
+            
+                <div class="pl-lg-4">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label dark"  for="input-username">User Name</label>
+                        <input type="text" id="input-username" required  class="form-control form-control-alternative"  value="Dilan Vasandaraj">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-email">Email Address</label>
+                        <input type="email" id="input-email" required class="form-control form-control-alternative" value="vasandarajdilan64@gmail.com">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-first-name">Contact No</label>
+                        <input type="text" id="input-first-name" required class="form-control form-control-alternative"  value="0717236676">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Password</label>
+                        <input type="password" id="input-last-name"  required class="form-control form-control-alternative"  value="************">
+                      </div>
+                    </div>
+                  </div>
+				  
+				  <div class="row">
+				   <div class="col-lg-3">
+                      <div class="form-group focused">
+                        
+                      </div>
+                    </div>
+					 
+                    <div class="col-lg-3">
+                      <div class="form-group focused">
+                         <button id="edit1" type="submit" class="btn btn-sm btn-success">Save Changes</button>
+                      </div>
+                    </div>
+                    <div class="col-lg-3">
+                      <div class="form-group focused">
+                     <button id="cac" class="btn btn-sm btn-danger">Cancel Changes</button>
+                      </div>
+                    </div>
+					<div class="col-lg-3">
+                      <div class="form-group focused">
+                        
+                      </div>
+                    </div>
+                  </div>
+				  
+				 
+				
+				 
+				   
+                </div>
+				</form>
+</div>
+</section>
+  </div>
+
+</div>
+        
     </section>
+	
+	<script>
+	
+
+// Get the customModal
+var customModal = document.getElementById("myModal");
+
+// Get the button that opens the customModal
+var btn = document.getElementById("myBtn");
+var edit = document.getElementById("edit1");
+var cac = document.getElementById("cac");
+
+
+// Get the <span> element that closes the customModal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the customModal 
+btn.onclick = function() {
+  customModal.style.display = "block";
+}
+edit.onclick = function() {
+  customModal.style.display = "none";
+}
+cac.onclick = function() {
+  customModal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the customModal
+span.onclick = function() {
+  customModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the customModal, close it
+window.onclick = function(event) {
+  if (event.target == customModal) {
+    customModal.style.display = "none";
+  }
+}
+</script>
+
 </body>
 <?php include 'footer.php'; ?>
 
